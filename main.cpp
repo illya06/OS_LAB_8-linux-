378b9fa4ccb37936b010c0dbc4690cb0639c7993
Ilya Shevchuk <il2001sh@gmail.com>
2020-12-20 14:00:47 +0200

    Changed tid output

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <iostream>
 #include <chrono>
+#include <sys/types.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -25,7 +27,7 @@ int main(){
     std::cin>>choise;
 
     if(choise == 1){
-        std::cout << "ENTER STEP VALUE : ";
+        std::cout << " Enter step value : ";
         double left = -0.9, right = -0.9 + (1.8 / amm), step;
         std::cin >> step;
 
@@ -42,7 +44,7 @@ int main(){
             right = right + (1.8 / amm);
         }
     }else if (choise == 0){
-        std::cout << "ENTER STEP VALUE : ";
+        std::cout << "Enter ammount of cycles : ";
         int cycles;
         std::cin>>cycles;
 
@@ -71,14 +73,14 @@ void* function_iter(void* param)
 {
     double* arr = (double*)param;
     double step = arr[0], left = arr[1], right = arr[2];
-    printf("\n (\033[32m%ld\033[0m) FUNCTION PARAMETERS: \n  step  :\033[33m %+.4f \033[0m \n  left  :\033[33m %+.4f \033[0m  \n  right :\033[33m %+.4f \033[0m\n", pthread_self(), step, left, right);
+    printf("\n (\033[32m%ld\033[0m) FUNCTION PARAMETERS: \n  step  :\033[33m %+.4f \033[0m \n  left  :\033[33m %+.4f \033[0m  \n  right :\033[33m %+.4f \033[0m\n", gettid(), step, left, right);
     double x;
     
     for (double i = left; i < right; i += step) {
         x = 1 + i / 3 - i * i / 9 + i * i * i * 5 / 81 - i * i * i * i * 80 / 1944;
-        printf("\n \033[36m %ld\033[0m -> X: %+.4f | Y: %+.4f ",pthread_self(), i, x);
+        printf("\n \033[36m %ld\033[0m -> X: %+.4f | Y: %+.4f ",gettid(), i, x);
     }
-    printf("\n\n (\033[32m%ld\033[0m) FINISHED!\n", pthread_self());
+    printf("\n\n (\033[32m%ld\033[0m) FINISHED!\n", gettid());
     return 0;
 
     pthread_exit(NULL);
@@ -88,9 +90,9 @@ void* function_id(void* param)
 {
     int iter = *(int*)param;
     for (int i = 0; i < iter; i++) {
-        printf("\n \033[36m %ld\033[0m -> Shevchuk Ilya #1245123451 ",pthread_self());
+        printf("\n \033[36m %ld\033[0m -> Shevchuk Ilya #1245123451 ",gettid());
     }
-    printf("\n\n (\033[32m%ld\033[0m) FINISHED!\n", pthread_self());
+    printf("\n\n (\033[32m%ld\033[0m) FINISHED!\n", gettid());
     return 0;
 
     pthread_exit(NULL);
